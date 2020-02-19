f14a1bdf5ffb29949f9cbbf64e58dd8440d30453
aurimas <auris.narkus@gmail.com>
2020-02-19 15:05:19 +0200

    Rebuilt dist

--- dist/bouncer.js
+++ dist/bouncer.js
@@ -1,7 +1,7 @@
 /*!
  * formbouncerjs v1.4.5
  * A lightweight form validation script that augments native HTML5 form validation elements and attributes.
- * (c) 2019 Chris Ferdinandi
+ * (c) 2020 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/bouncer
  */
@@ -272,15 +272,22 @@
 	 * @param  {Object} errors      The existing errors
 	 * @param  {Object} validations The custom validations to run
 	 * @param  {Object} settings    The plugin settings
-	 * @return {Object}             The tests and their results
+	 * @return {Promise<Object>}    The tests and their results
 	 */
 	var customValidations = function (field, errors, validations, settings) {
+		var getCustomErrors = [];
 		for (var test in validations) {
 			if (validations.hasOwnProperty(test)) {
-				errors[test] = validations[test](field, settings);
+				var getCustomError = validations[test](field, settings).then((function (error) {
+					errors[test] = error;
+				}));
+
+				getCustomErrors.push(getCustomError);
 			}
 		}
-		return errors;
+		return Promise.all(getCustomErrors).then((function () {
+			return errors;
+		}));
 	};
 
 	/**
@@ -299,7 +306,7 @@
 	 * Check a field for errors
 	 * @param  {Node} field      The field to test
 	 * @param  {Object} settings The plugin settings
-	 * @return {Object}          The field validity and errors
+	 * @return {Promise<Object>} The field validity and errors
 	 */
 	var getErrors = function (field, settings) {
 
@@ -307,13 +314,12 @@
 		var errors = runValidations(field,settings);
 
 		// Check for custom validations
-		errors = customValidations(field, errors, settings.customValidations, settings);
-
-		return {
-			valid: !hasErrors(errors),
-			errors: errors
-		};
-
+		return customValidations(field, errors, settings.customValidations, settings).then((function (errors) {
+			return {
+				valid: !hasErrors(errors),
+				errors: errors
+			};
+		}));
 	};
 
 	/**
@@ -456,6 +462,11 @@
 
 		// If the message should come after the field
 		if (settings.messageAfterField) {
+			// If there's no next sibling, create one
+			if (!target.nextSibling) {
+				target.parentNode.appendChild(document.createTextNode(''));
+			}
+
 			return target.nextSibling;
 		}
 
@@ -682,43 +693,48 @@
 
 		/**
 		 * Validate a field
-		 * @param  {Node} field     The field to validate
-		 * @param  {Object} options Validation options
-		 * @return {Object}         The validity state and errors
+		 * @param  {Node} field      The field to validate
+		 * @param  {Object} options  Validation options
+		 * @return {Promise<Object>} The validity state and errors
 		 */
 		publicAPIs.validate = function (field, options) {
 
 			// Don't validate submits, buttons, file and reset inputs, and disabled and readonly fields
-			if (field.disabled || field.readOnly || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
+			if (field.disabled || field.readOnly || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return Promise.resolve();
 
 			// Local settings
 			var _settings = extend(settings, options || {});
 
 			// Check for errors
-			var isValid = getErrors(field, _settings);
+			return getErrors(field, _settings).then((function(isValid) {
 
-			// If valid, remove any error messages
-			if (isValid.valid) {
-				removeError(field, _settings);
-				return;
-			}
+				// If valid, remove any error messages
+				if (isValid.valid) {
+					removeError(field, _settings);
+					return;
+				}
 
-			// Otherwise, show an error message
-			showError(field, isValid.errors, _settings);
+				// Otherwise, show an error message
+				showError(field, isValid.errors, _settings);
 
-			return isValid;
-
+				return isValid;
+			}));
 		};
 
 		/**
 		 * Validate all fields in a form or section
-		 * @param  {Node} target The form or section to validate fields in
-		 * @return {Array}       An array of fields with errors
+		 * @param  {Node} target    The form or section to validate fields in
+		 * @return {Promise<Array>} An array of fields with errors
 		 */
 		publicAPIs.validateAll = function (target) {
-			return Array.prototype.filter.call(target.querySelectorAll('input, select, textarea'), (function (field) {
-				var validate = publicAPIs.validate(field);
-				return validate && !validate.valid;
+			var getValidations = Array.prototype.map.call(target.querySelectorAll('input, select, textarea'), (function (field) {
+				return publicAPIs.validate(field).then((function (validate) {
+					return validate && !validate.valid ? field : null;
+				}));
+			}));
+
+			return Promise.all(getValidations).then((function (validations) {
+				return validations.filter(Boolean);
 			}));
 		};
 
@@ -763,24 +779,25 @@
 			event.preventDefault();
 
 			// Validate each field
-			var errors = publicAPIs.validateAll(event.target);
+			publicAPIs.validateAll(event.target).then((function (errors) {
 
-			// If there are errors, focus on the first one
-			if (errors.length > 0) {
-				errors[0].focus();
-				emitEvent(event.target, 'bouncerFormInvalid', {errors: errors});
-				return;
-			}
+				// If there are errors, focus on the first one
+				if (errors.length > 0) {
+					errors[0].focus();
+					emitEvent(event.target, 'bouncerFormInvalid', {errors: errors});
+					return;
+				}
 
-			// Otherwise, submit if not disabled
-			if (!settings.disableSubmit) {
-				event.target.submit();
-			}
+				// Otherwise, submit if not disabled
+				if (!settings.disableSubmit) {
+					event.target.submit();
+				}
 
-			// Emit custom event
-			if (settings.emitEvents) {
-				emitEvent(event.target, 'bouncerFormValid');
-			}
+				// Emit custom event
+				if (settings.emitEvents) {
+					emitEvent(event.target, 'bouncerFormValid');
+				}
+			}));
 
 		};
 
